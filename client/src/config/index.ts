34072898edde341edd81e59873c217888dae7628
Jon Hemstreet <jon.hemstreet@unanet.com>
2021-06-01 11:54:03 -0600

    code to update widndow location

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -2,8 +2,8 @@ class Config {
     API_URL: string
     READ_ONLY: boolean
     constructor() {
-        this.API_URL = process.env.VUE_APP_API_URL || "http://localhost:8081/api";
-        this.READ_ONLY = (process.env.VUE_APP_READ_ONLY) ? JSON.parse(process.env.VUE_APP_READ_ONLY) : false;
+        this.API_URL = process.env.VUE_APP_API_URL || `${document.location.protocol}//${document.location.host}/api`;
+        this.READ_ONLY = (process.env.VUE_APP_READ_ONLY) ? JSON.parse(process.env.VUE_APP_READ_ONLY) : true;
     }
     isReadOnly(): boolean {
         return this.READ_ONLY;
